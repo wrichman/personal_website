10e0ed7ebb8fa0f09d3401ea8ce9abe1618bbb14
Will Richman <williamrichman@gmail.com>
2013-11-09 17:19:55 -0500

    Refactoring alerts and footer into partials. Adding index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   skip_before_filter :verify_authenticity_token  
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
