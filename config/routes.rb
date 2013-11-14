e43085fdbd16e890ec67e2cf17beb4a13b1b9cdd
Will Richman <williamrichman@gmail.com>
2013-11-14 07:06:45 -0500

    Changing root url, fixing new course and lesson buttons

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ PersonalWebsite::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'welcome#index'
+  root :to => 'users#index'
 
 
   resources :sessions, :only => [:new, :create, :destroy]
