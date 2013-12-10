1c2772bba2e7021c47b27f3856ceb74ef14efcb2
Will Richman <williamrichman@gmail.com>
2013-12-09 21:03:27 -0500

    Minor changes to relationship model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,7 +1,5 @@
 class Relationship < ActiveRecord::Base
   belongs_to :creator, class_name: "User"
   belongs_to :subscriber, class_name: "User"
-  belongs_to :course
-
-  
+  belongs_to :course  
 end
